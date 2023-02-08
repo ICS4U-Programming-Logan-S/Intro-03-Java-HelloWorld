31c789c98566430763a9276cc063c7c8a69020dd
Logan Sweeney <logan.sweeney@stu.ocsb.ca>
2023-02-08 19:06:19 +0000

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+//creates the object
+class Main {
+  public static void main(String[] args) {
+    System.out.println ("What's up buttercup"); // prints What's up buttercup
+    System.out.println ("Rodents of unusual size? I don't think they exist."); // prints a sentence
+  }
+}
