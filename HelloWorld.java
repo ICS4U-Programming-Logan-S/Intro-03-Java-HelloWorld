f0b74adc81b5a88f6c2ed0289f39b3b58de04d99
Logan Sweeney <logan.sweeney@stu.ocsb.ca>
2023-02-14 19:12:41 +0000

    Finished Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,22 @@
-//creates the text object
-class Main {
-  public static void main(String[] args) {
-    // prints What's up buttercup
-    System.out.println ("What's up buttercup");
-    // prints a sentence
-    System.out.println ("Rodents of unusual size? I don't think they exist.");
-  }
+/**
+* Says Hello World.
+*
+* @author  Logan S
+* @version 1.0
+* @since   2023-02-14
+*/
+public class HelloWorld {
+    /**
+     * This is the main method.
+     *
+     * @param args
+     *
+     */
+    public static void main(String[] args) {
+        // prints What's up buttercup
+        System.out.println("What's up buttercup");
+        // prints a sentence
+        System.out.print("Rodents of unusual size? ");
+        System.out.println("I don't think they exist.");
+    }
 }
